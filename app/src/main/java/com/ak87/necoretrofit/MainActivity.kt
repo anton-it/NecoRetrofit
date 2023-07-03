fd023ac7f25b0b87449c1404c568c8e84178cd4e
Admin1 <a89086829981@gmail.com>
2023-07-03 13:00:39 +0300

    Add dependencies Add retrofit instance

--- app/src/main/java/com/ak87/necoretrofit/MainActivity.kt
+++ app/src/main/java/com/ak87/necoretrofit/MainActivity.kt
@@ -2,10 +2,37 @@ package com.ak87.necoretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.ak87.necoretrofit.retrofit.ProductApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tv = findViewById<TextView>(R.id.tv)
+        val button = findViewById<Button>(R.id.button)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://dummyjson.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val productApi = retrofit.create(ProductApi::class.java)
+
+        button.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val product = productApi.getProductById(3)
+                runOnUiThread {
+                    tv.text = product.title
+                }
+            }
+        }
     }
 }
