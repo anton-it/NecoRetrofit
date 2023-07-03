ff73ba257e587f35e5e1747306c9e001d22ad431
Admin1 <a89086829981@gmail.com>
2023-07-03 09:54:19 +0300

    Initial commit

---
+++ app/src/main/java/com/ak87/necoretrofit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ak87.necoretrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
